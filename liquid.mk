60ce2a204dfb96794c7c9d9ef6f60dc662df42b8
dhacker29 <davidhackerdvm@gmail.com>
2013-02-11 01:09:20 -0600

    Liquify

---
+++ liquid.mk
@@ -0,0 +1,38 @@
+# Inherit device configuration for Droid Bionic.
+$(call inherit-product, device/motorola/targa/full_targa.mk)
+
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Inherit some common CDMA stuff.
+$(call inherit-product, vendor/liquid/config/common_cdma.mk)
+
+# CM-specific init file for SafeStrap
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/etc/init.liquid.rc:system/etc/rootfs/init.liquid.rc
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := liquid_targa
+PRODUCT_BRAND := verizon
+PRODUCT_DEVICE := targa
+PRODUCT_DEVICE_PREFIX := cdma
+PRODUCT_MODEL := XT875
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := DROID_BIONIC
+PRODUCT_SFX := vzw
+
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=${PRODUCT_DEVICE}_${PRODUCT_SFX} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE} \
+   BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}_${PRODUCT_SFX}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+   PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+   PRODUCT_BRAND=${PRODUCT_BRAND} \
+   BUILD_UTC_DATE= \
+   PRODUCT_DEFAULT_LANGUAGE=en \
+   PRODUCT_DEFAULT_REGION=US \
